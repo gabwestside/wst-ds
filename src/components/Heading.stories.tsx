f136e621a08ee24ac7e876ad3d9131b0c0be6e6e
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-10-22 23:44:09 -0300

    creating Heading and Button components

--- src/components/Heading.stories.tsx
+++ src/components/Heading.stories.tsx
@@ -1,10 +1,10 @@
 import { Meta, StoryObj } from '@storybook/react'
 
-import { Text, TextProps } from './Text'
+import { Heading, HeadingProps } from './Heading'
 
 export default {
-  title: 'Component/Text',
-  component: Text,
+  title: 'Component/Heading',
+  component: Heading,
   args: {
     children: 'Lorem ipsum.',
     size: 'md'
@@ -17,27 +17,27 @@ export default {
       }
     }
   }
-} as Meta<TextProps>
+} as Meta<HeadingProps>
 
-export const Default: StoryObj<TextProps> = {}
+export const Default: StoryObj<HeadingProps> = {}
 
-export const Small: StoryObj<TextProps> = {
+export const Small: StoryObj<HeadingProps> = {
   args: {
     size: 'sm'
   }
 }
 
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
   args: {
     size: 'lg'
   }
 }
 
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
   args: {
     asChild: true,
     children: (
-      <p>Text with paragraph</p>
+      <h1>Heading with H1</h1>
     )
   },
   argTypes: {
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,6 +12,9 @@ module.exports = {
     },
 
     colors: {
+      black: '#000',
+      white: '#FFF',
+
       'gray-900': '#121214',
       'gray-800': '#202024',
       'gray-400': '#7c7c8a',
