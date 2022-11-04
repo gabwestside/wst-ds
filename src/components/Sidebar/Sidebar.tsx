d22c17778929cbb0102e6c66b3ca98c0c8b783b4
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-11-03 23:25:23 -0300

    adding new actions to sidebar component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,28 +1,60 @@
 import { useState } from 'react'
-import { Atom } from 'phosphor-react'
+import { clsx } from 'clsx'
+import { Atom, ArrowSquareLeft, ArrowSquareRight, User } from 'phosphor-react'
+
+import { Text } from '../Text/Text'
+import { Heading } from '../Heading/Heading'
 
 interface SidebarProps {}
 
 export const Sidebar = ({}: SidebarProps) => {
   const [isOpen, setIsOpen] = useState(true)
+  const ArrowIcon = isOpen ? ArrowSquareLeft : ArrowSquareRight
+  const mainLogoTransition = isOpen ? 'default' : 'smallPrimary'
+  const navButtonSize = isOpen ? 'medium' : 'small'
 
   return (
-    <div className='w-full'>
-      <div>
-        <Atom className='text-cyan-500 h-8 w-8' />
+    <div
+      className={clsx('h-full bg-gray-800 transition duration-150 ease-out', {
+        'w-[320px]': isOpen,
+        'w-16': !isOpen,
+      })}
+    >
+      <div
+        className={clsx(' flex justify-center items-center gap-1', {
+          'w-36': isOpen,
+          'w-14 ': !isOpen,
+        })}
+      >
+        <Atom
+          className={clsx('text-cyan-500 my-5', {
+            'h-10 w-10': isOpen,
+            'h-8 w-8': !isOpen,
+          })}
+        />
+        {isOpen && <Heading size='sm'>wstside</Heading>}
       </div>
 
-      <div onClick={() => setIsOpen(!isOpen)}>
-        <img />
-        {isOpen && <caption>Reduzir</caption>}
+      <div
+        onClick={() => setIsOpen(!isOpen)}
+        className={clsx(
+          'flex items-center m-3 gap-3 font-medium cursor-pointer',
+          {
+            'justify-end': isOpen,
+            'justify-center': !isOpen,
+          }
+        )}
+      >
+        <ArrowIcon size={24} className='text-white' />
+        {isOpen && <Text size='sm'>Reduce</Text>}
       </div>
 
-      {/* <Styled.AccountDetail onClick={() => setIsOpen(!isOpen)} isClosed={!isOpen}>
-        <Icon name='user' width={24} height={24} />
+      <div onClick={() => setIsOpen(!isOpen)} className={clsx('')}>
+        <User size={24} className='text-white' />
         {isOpen && 'User Detail component'}
-      </Styled.AccountDetail>
+      </div>
 
-      <Styled.ButtonsGrid isClosed={!isOpen}>
+      {/* <Styled.ButtonsGrid isClosed={!isOpen}>
         <NavButton onClick={() => setIsOpen} size={navButtonSize} icon='moneyTransfer'>
           Transferencia
         </NavButton>
@@ -41,9 +73,9 @@ export const Sidebar = ({}: SidebarProps) => {
         <NavButton onClick={() => setIsOpen} size={navButtonSize} icon='cardMulti'>
           Cartões
         </NavButton>
-      </Styled.ButtonsGrid>
+      </Styled.ButtonsGrid> */}
 
-      <Styled.CloseNavBar isClosed={!isOpen}>
+      {/* <Styled.CloseNavBar isClosed={!isOpen}>
         <Icon name='cancel' width={24} height={24} />
         {isOpen && <Typography variant='caption'>Sair</Typography>}
       </Styled.CloseNavBar> */}
