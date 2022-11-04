bea62c7c85ef57cc24a3c43bbb8a0a495247475e
Gabriel <gabrielnfl13@gmail.com>
2022-11-03 21:01:48 -0300

    atualizações no componente de sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,7 +1,52 @@
 import { useState } from 'react'
+import { Atom } from 'phosphor-react'
 
-export interface SidebarProps {}
+interface SidebarProps {}
 
-export function Sidebar({}: SidebarProps) {
-  return <h1>Hello World</h1>
+export const Sidebar = ({}: SidebarProps) => {
+  const [isOpen, setIsOpen] = useState(true)
+
+  return (
+    <div className='w-full'>
+      <div>
+        <Atom className='text-cyan-500 h-8 w-8' />
+      </div>
+
+      <div onClick={() => setIsOpen(!isOpen)}>
+        <img />
+        {isOpen && <caption>Reduzir</caption>}
+      </div>
+
+      {/* <Styled.AccountDetail onClick={() => setIsOpen(!isOpen)} isClosed={!isOpen}>
+        <Icon name='user' width={24} height={24} />
+        {isOpen && 'User Detail component'}
+      </Styled.AccountDetail>
+
+      <Styled.ButtonsGrid isClosed={!isOpen}>
+        <NavButton onClick={() => setIsOpen} size={navButtonSize} icon='moneyTransfer'>
+          Transferencia
+        </NavButton>
+        <NavButton onClick={() => setIsOpen} size={navButtonSize} icon='pix'>
+          Cartões
+        </NavButton>
+        <NavButton onClick={() => setIsOpen} size={navButtonSize} icon='payment'>
+          Pagamento
+        </NavButton>
+        <NavButton onClick={() => setIsOpen} size={navButtonSize} icon='topup'>
+          Recarga
+        </NavButton>
+        <NavButton onClick={() => setIsOpen} size={navButtonSize} icon='calendar'>
+          Agendamento
+        </NavButton>
+        <NavButton onClick={() => setIsOpen} size={navButtonSize} icon='cardMulti'>
+          Cartões
+        </NavButton>
+      </Styled.ButtonsGrid>
+
+      <Styled.CloseNavBar isClosed={!isOpen}>
+        <Icon name='cancel' width={24} height={24} />
+        {isOpen && <Typography variant='caption'>Sair</Typography>}
+      </Styled.CloseNavBar> */}
+    </div>
+  )
 }
